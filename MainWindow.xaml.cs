68770c88e6b943c998a86639972f0483c3b6cf54
Yuxuan Wei🚚 <yuxwei@microsoft.com>
2026-01-12 17:14:19 +0800

    feat: 迁移到 WPF UI 框架，打造 Windows 11 风格界面
    - 替换 MaterialDesign 为 WPF-UI 4.2.0
    - 使用 FluentWindow 支持 Mica 背景效果
    - 重新设计聊天界面，苹果风格消息气泡
    - 升级到 .NET 8.0
    - 现代化控件：Card、Badge、InfoBar 等

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,41 +1,18 @@
 using System.Windows;
 using LocalKnowledgeBase.ViewModels;
+using Wpf.Ui.Controls;
 
 namespace LocalKnowledgeBase
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainViewModel();
-        }
-
-        private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-            }
-            else
-            {
-                DragMove();
-            }
-        }
-
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
+            
+            // 应用 Mica 背景效果
+            Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
         }
     }
 }
